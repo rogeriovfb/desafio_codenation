12b12bf0d041632f77ec95bd4b318086845b3116
Rogerio Ballestrin <ballestrin.rogerio@gmail.com>
2020-06-28 18:51:23 -0300

    Código final

--- cesar.py
+++ cesar.py
@@ -25,14 +25,8 @@ json_data['resumo_criptografico'] = hashlib.sha1(decif.encode('utf-8')).hexdiges
 with open('answer.json', 'w') as outfile:
     json.dump(json_data, outfile)
 
-""" Seu programa deve submeter o arquivo atualizado para correção via POST para a API:
-https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=SEU_TOKEN
-OBS: a API espera um arquivo sendo enviado como multipart/form-data, como se fosse enviado por um formulário HTML, 
-com um campo do tipo file com o nome answer. Considere isso ao enviar o arquivo.
-"""
-
 r = requests.post(
-    'http://httpbin.org/post',
+    'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=17597f8135080613ca38102063f6b1e6a3b418a1',
     files={'answer': open('answer.json', 'rb')}
 )
 print(r.content)
